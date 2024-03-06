fdb9ebd12658009cd038b810e630d72bcdac8c3c
Michelly Darquia <michelly.13darquia@gmail.com>
2024-03-06 18:02:21 -0300

    upload e download

--- src/pages/Exame.tsx
+++ src/pages/Exame.tsx
@@ -1,22 +1,35 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 
 const Exames: React.FC = () => {
+  const handleDownloadClick = () => {
+    // lógica
+    const urlDoExame = 'URL_DO_EXAME_AQUI';
 
-    return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Page Title</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent className="ion-padding">
+    // Iniciar o download
+    const link = document.createElement('a');
+    link.href = urlDoExame;
+    link.target = '_blank';
+    link.download = 'nome_do_arquivo_do_exame';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
-                <IonButton fill="clear" >BAIXAR RESULTADO DO EXAME</IonButton>
-
-            </IonContent>
-        </IonPage>
-    );
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Page Title</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <IonButton fill="clear" onClick={handleDownloadClick}>
+          BAIXAR RESULTADO DO EXAME
+        </IonButton>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default Exames;
--- src/pages/perfilPacienteProfissional.tsx
+++ src/pages/perfilPacienteProfissional.tsx
@@ -1,7 +1,27 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonAvatar, IonButton } from '@ionic/react';
-import React from 'react';
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonAvatar, IonButton, IonIcon, IonCardContent, IonInput } from '@ionic/react';
+import { cloudUploadOutline } from 'ionicons/icons';
 
-const perfilPacienteProfissional: React.FC = () => {
+const PerfilPacienteProfissional: React.FC = () => {
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const files = event.target.files;
+        if (files && files.length > 0) {
+            setSelectedFile(files[0]);
+        }
+    }; // abrindo os meu arquivos opara escolher o arquivo a ser enviado
+
+    const handleUpload = () => {
+        // Lógica
+        if (selectedFile) {
+            // Lógica para onde o uploud vai
+            console.log("Arquivo selecionado:", selectedFile.name);
+
+        } else {
+            console.log("Nenhum arquivo selecionado.");
+        }
+    };
 
     return (
         <>
@@ -9,25 +29,31 @@ const perfilPacienteProfissional: React.FC = () => {
                 <IonHeader>
                     <IonToolbar>
                         <IonAvatar>
-                            <img alt="Imagem do perfil"
-                            src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+                            <img alt="Imagem do perfil" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
                         </IonAvatar>
                         <IonTitle>.................NOME COMPLETO</IonTitle>
                     </IonToolbar>
                 </IonHeader>
                 <IonContent className="ion-padding">
-                    <br/>
-                    DADOS: <br/>
-                    DADOS: <br/>
-                    DADOS: <br/>
-                    DADOS: <br/>
-                    DADOS: <br/>
+                    <br />
+                    DADOS: <br />
+                    DADOS: <br />
+                    DADOS: <br />
+                    DADOS: <br />
+                    DADOS: <br />
                     <IonButton color={'success'}>Exames</IonButton>
                     <IonButton color={'success'}>Marcar exames</IonButton>
+
+                    <IonCardContent>
+                        <IonIcon icon={cloudUploadOutline} size="large" />
+                        <IonInput type="file" onInput={handleFileChange} />
+                        <IonButton onClick={handleUpload}>Enviar Arquivo</IonButton>
+                    </IonCardContent>
                 </IonContent>
             </IonPage>
         </>
     );
 };
 
-export default perfilPacienteProfissional;
+export default PerfilPacienteProfissional;
+
