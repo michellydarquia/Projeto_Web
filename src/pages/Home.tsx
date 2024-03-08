3eebb65b109964540e0374f975ccec9232e56707
BeaBia <biabandeira10@gmail.com>
2024-03-08 01:47:43 -0300

    Home quase ok

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,10 +11,11 @@ import {
     IonRow,
     IonCol,
     IonCard,
+    IonText,
 } from '@ionic/react';
 import React from 'react';
 import '../theme/home.css';
-import schImg from '../imagens/sch.png';
+import schImg from '../imagens/sch-removebg.png';
 
 
 import * as shared from '../shared'
@@ -31,7 +32,7 @@ const Home: React.FC = () => {
                         <IonRow>
                             <IonCol class='colImg'>
                                 <div className="container">
-                                    <IonTitle id='introText'>Acompanhe o resultado dos exames feitos</IonTitle>
+                                    <IonTitle id='introText'>Acesse o resultado de seus exames aqui</IonTitle>
                                     <div className='image-container'>
                                         <IonImg src={schImg} id="imagem" class='ion-float-left'/>
 
@@ -40,14 +41,27 @@ const Home: React.FC = () => {
                             </IonCol>
                             <IonCol class='colBtt'>
                                 <IonCard id='card'>
+                                <IonRow>
+                                        <IonTitle id='loginTitle'>
+                                            <h2>Escolha seu tipo de login:</h2>
+                                        </IonTitle>
+                                    </IonRow>
                                 <div id='buttonsDiv' className='ion-text-right'>
+                                    
                                     <IonRow id='rowPac'>
                                     <IonButton href='/login' id="bttPac" 
                                     className='button' 
                                     expand='block'
                                     shape='round'
                                     >Login como paciente</IonButton>
-                                    
+                                    </IonRow>
+                                    <IonRow id='loginText'>
+                                        <div id='linha_separacao'></div>
+                                        <IonText>
+                                            <p>OU</p>
+                                        </IonText>
+                                        <div id='linha_separacao'></div>
+
                                     </IonRow>
                                     <IonRow>
                                     <IonButton href='/login' id="bttMed" 
